e833e22b9b44337df4781cdb7e25df6ccb0e1cac
sgr-6 <appisagar25@gmail.com>
2026-05-30 22:41:25 +0530

    Fix service worker caching issue causing blank screens

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,17 @@
-const CACHE_NAME = 'attendance-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(() => {
+      self.registration.unregister();
+    })
   );
 });
